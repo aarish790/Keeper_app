aea22b76a08ba67f13a15e9d098c4ba3bf7268be
aarish790 <60058807+aarish790@users.noreply.github.com>
2020-06-27 14:36:29 +0530

    second

---
+++ build/precache-manifest.7894069795570245ab3b34779ef689dc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fe856fc9c9a89228f01e3d4ef16679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "499a1e7351c830159f13",
+    "url": "/static/js/2.f5de51ba.chunk.js"
+  },
+  {
+    "revision": "27da303d114126300057",
+    "url": "/static/js/main.24a1e4d5.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
